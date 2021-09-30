47ef05cd6e6a10c6a16e840d27d6727b9a5db0f7
Gamma_Sigma <lateo1234@gmail.com>
2021-09-30 22:09:39 +0200

    **Master** - Cambiato delay e colore background + tabs

--- 01_Hello_SDL/01_hello_SDL.cpp
+++ 01_Hello_SDL/01_hello_SDL.cpp
@@ -1,7 +1,7 @@
 /**
  * @file 01_hello_SDL.cpp
  * @brief https://lazyfoo.net/tutorials/SDL/01_hello_SDL/windows/mingw/
- * 
+ *
  * - Scaricare SDL Development Libraries (versione MinGW)
  * - Estrarre l'archivio in una cartella a propria scelta
  * - Per MinGW-w64, utilizzeremo la versione x86_64-w64-mingw32 di SDL (non la i686)
@@ -26,46 +26,46 @@ const int SCREEN_HEIGHT = 768;
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+  //The window we'll be rendering to
+  SDL_Window* window = NULL;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+  //The surface contained by the window
+  SDL_Surface* screenSurface = NULL;
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+  }
+  else
+  {
+    //Create window
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( window == NULL )
+    {
+      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+      //Get window surface
+      screenSurface = SDL_GetWindowSurface( window );
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-		}
-	}
+      //Fill the surface white
+      SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0x00 ) );
 
-	//Destroy window
-	SDL_DestroyWindow( window );
+      //Update the surface
+      SDL_UpdateWindowSurface( window );
 
-	//Quit SDL subsystems
-	SDL_Quit();
+      //Wait two seconds
+      SDL_Delay( 5000 );
+    }
+  }
 
-	return 0;
+  //Destroy window
+  SDL_DestroyWindow( window );
+
+  //Quit SDL subsystems
+  SDL_Quit();
+
+  return 0;
 }

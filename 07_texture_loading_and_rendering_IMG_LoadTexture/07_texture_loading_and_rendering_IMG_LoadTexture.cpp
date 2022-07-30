d32c5caa43a32ce8aa789019a4b5fcbc91a3a8cc
Gamma_Sigma <lateo1234@gmail.com>
2022-07-30 22:23:40 +0200

    **Master** - Modificato esempio 07 + commenti
    Sdoppiato esempio 07 in versione originale, senza `IMG_LoadTexture`, e versione con `IMG_LoadTexture`.

--- 07_texture_loading_and_rendering_IMG_LoadTexture/07_texture_loading_and_rendering_IMG_LoadTexture.cpp
+++ 07_texture_loading_and_rendering_IMG_LoadTexture/07_texture_loading_and_rendering_IMG_LoadTexture.cpp
@@ -1,73 +1,7 @@
 /**
- * @file 07_texture_loading_and_rendering.cpp
+ * @file 07_texture_loading_and_rendering_IMG_LoadTexture.cpp
  *
- * @brief This source code copyrighted by Lazy Foo' Productions (2004-2022)
- * and may not be redistributed without written permission.
- *
- * Textures in SDL have their own data type, intuitively called an "SDL_Texture". When we deal with
- * SDL textures, you need an SDL_Renderer to render it to the screen, which is why we declare a
- * global renderer named "gRenderer".
- *
- * We have a new image loading routine ("loadTexture") and a globally declared texture ("gTexture")
- * we're going to load.
- *
- * After we create our window, we have to create a renderer for our window, so we can render textures
- * on it. This is easily done with a call to "SDL_CreateRenderer".
- *
- * After creating the renderer, we want to initialize the rendering color using
- * "SDL_SetRenderDrawColor". This controls what color is used for various rendering operations.
- *
- * Our texture loading function looks largely the same as the earlier "loadSurface", only now
- * instead of converting the loaded surface to the display format, we create a texture from the
- * loaded surface using "SDL_CreateTextureFromSurface". As before, this function creates a new
- * texture from an existing surface, which means like before we have to free the loaded surface and
- * then return the loaded texture.
- *
- * Since texture loading is abstracted with our image loading function, the "loadMedia" function
- * works pretty much the same as before.
- *
- * In our clean up function, we have to remember to deallocate our textures using
- * "SDL_DestroyTexture".
- *
- * In the main loop after the event loop, we call "SDL_RenderClear". This function fills the screen
- * with the color that was last set with "SDL_SetRenderDrawColor".
- *
- * With the screen cleared, we render the texture with "SDL_RenderCopy". With the texture rendered, we
- * still have to update the screen, but since we're not using "SDL_Surfaces" to render, we can't use
- * "SDL_UpdateWindowSurface". Instead we have to use "SDL_RenderPresent".
- *
- * Now, there is a new API call called "IMG_LoadTexture". It's not in the official documentation as of
- * this writing, but you can find it in the SDL_image header files. It allows you to load a texture
- * without having to create a temporary surface. The reason I haven't gone back and updated the
- * tutorial to use this method is because I don't want to go back an update 40+ tutorials. Even
- * though it is undocumented, it works just fine.
- *
- * Sunto:
- *
- * main
- * |
- * |----init
- * |    |
- * |    |----SDL_Init
- * |    |----SDL_SetHint
- * |    |----SDL_CreateWindow
- * |    |----SDL_CreateRenderer
- * |    |----SDL_SetRenderDrawColor
- * |    |----IMG_Init
- * |
- * |----loadMedia
- * |    |
- * |    |----loadTexture
- * |         |
- * |         |----IMG_Load
- * |         |----SDL_CreateTextureFromSurface
- * |         |----SDL_FreeSurface
- * |
- * |----SDL_PollEvent
- * |----SDL_RenderClear
- * |----SDL_RenderCopy
- * |----SDL_RenderPresent
- * |----close
+ * @brief Come il precedente, ma con l'uso di "IMG_LoadTexture".
  **/
 
 
@@ -246,41 +180,26 @@ static void close(void)
 
 
 /**
- * @brief Loads individual image as texture.
+ * @brief Loads individual image as texture. Versione modificata rispetto all'altro esempio 07. Fa
+ * uso di "IMG_LoadTexture", che risparmia di dover creare una texture temporanea per poi
+ * eliminarla.
  *
- * @param path The path to the image
- * @return SDL_Surface* Pointer to the loaded image
+ * @param path The path to the image.
+ * @return SDL_Surface* Pointer to the loaded image.
  **/
 static SDL_Texture* loadTexture( std::string path )
 {
-  // The final texture
   SDL_Texture* newTexture = NULL;
 
-  // Load image at specified path
-  SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+  newTexture = IMG_LoadTexture( gRenderer, path.c_str() );
 
-  if( loadedSurface == NULL )
+  if( newTexture == NULL )
   {
-    printf( "\nUnable to load image \"%s\"! SDL_image Error: \"%s\"\n", path.c_str(), IMG_GetError() );
+    printf( "\nUnable to create texture from \"%s\"! SDL Error: \"%s\"\n", path.c_str(), SDL_GetError() );
   }
   else
   {
-    printf( "\nImage \"%s\" loaded", path.c_str() );
-
-    // Create texture from surface pixels
-    newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-
-    if( newTexture == NULL )
-    {
-      printf( "\nUnable to create texture from \"%s\"! SDL Error: \"%s\"\n", path.c_str(), SDL_GetError() );
-    }
-    else
-    {
-      printf( "\nTexture created from \"%s\"", path.c_str() );
-    }
-
-    // Get rid of old loaded surface
-    SDL_FreeSurface( loadedSurface );
+    printf( "\nTexture created from \"%s\"", path.c_str() );
   }
 
   return newTexture;
